51b4eb7aceb92b7eb80517dfd3cd47ed5731fe79
jia <657688353@qq.com>
2023-12-05 22:06:16 +0800

    文章管理

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,42 +1,111 @@
 <script setup>
+import ChannelSelect from './components/ChannelSelect.vue'
+import { ref } from 'vue'
+import ArticleEdit from './components/ArticleEdit.vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { artGetListService } from '@/api/article'
+import { formatTime } from '@/utils/format.js'
+
+const articleList = ref([]) // 文章列表
+const total = ref(0) // 总条数
+const loading = ref(false) // loading状态
+// 定义请求参数对象,也是表单的内容
+const params = ref({
+  pagenum: 1, // 当前页
+  pagesize: 5, // 当前生效的每页条数
+  cate_id: '',
+  state: ''
+})
+// 基于params参数，获取文章列表
+const getArticleList = async () => {
+  loading.value = true
+
+  const res = await artGetListService(params.value)
+  articleList.value = res.data.data
+  total.value = res.data.total
+
+  loading.value = false
+}
+// 一进页面先获取一遍数据
+getArticleList()
+const articleEditRef = ref()
+// 新增文章
+const onAddArticle = () => {
+  articleEditRef.value.open({})
+  console.log('Add article clicked')
+}
 const onEditArticle = (row) => {
+  articleEditRef.value.open({ row })
   console.log(row)
 }
 const onDeleteArticle = (row) => {
   console.log(row)
 }
+
+// 分页
+const onSizeChange = (size) => {
+  // console.log('当前每页条数', size)
+  // 只要是每页条数变化了，那么原本正在访问的当前页意义不大了，数据大概率已经不在原来那一页了
+  // 重新从第一页渲染即可
+  params.value.pagenum = 1
+  params.value.pagesize = size
+  // 基于最新的当前页 和 每页条数，渲染数据
+  getArticleList()
+}
+const onCurrentChange = (page) => {
+  // 更新当前页
+  params.value.pagenum = page
+  // 基于最新的当前页，渲染数据
+  getArticleList()
+}
+
+// 搜索逻辑 => 按照最新的条件，重新检索，从第一页开始展示
+const onSearch = () => {
+  params.value.pagenum = 1 // 重置页面
+  getArticleList()
+}
+
+// 重置逻辑 => 将筛选条件清空，重新检索，从第一页开始展示
+const onReset = () => {
+  params.value.pagenum = 1 // 重置页面
+  params.value.cate_id = ''
+  params.value.state = ''
+  getArticleList()
+}
 </script>
 <template>
   <page-container title="文章管理">
     <template #extra>
-      <el-button type="primary">发布文章</el-button>
+      <el-button type="primary" @click="onAddArticle">发布文章</el-button>
     </template>
     <el-form inline>
       <el-form-item label="文章分类：">
-        <el-select>
-          <el-option label="新闻" value="111"></el-option>
-          <el-option label="体育" value="222"></el-option>
-        </el-select>
+        <ChannelSelect v-model="params.cate_id"></ChannelSelect>
       </el-form-item>
       <el-form-item label="发布状态：">
-        <el-select>
+        <el-select v-model="params.state" placeholder="请选择文章状态">
           <el-option label="已发布" value="已发布"></el-option>
           <el-option label="草稿" value="草稿"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">搜索</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="onSearch">搜索</el-button>
+        <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
-    <el-table :data="articleList" style="width: 100%">
+    <el-table :data="articleList" style="width: 100%" v-loading="loading">
       <el-table-column label="文章标题" width="400">
         <template #default="{ row }">
           <el-link type="primary" :underline="false">{{ row.title }}</el-link>
         </template>
       </el-table-column>
       <el-table-column label="分类" prop="cate_name"></el-table-column>
-      <el-table-column label="发表时间" prop="pub_date"> </el-table-column>
+      <el-table-column label="发表时间" prop="pub_date">
+        <!-- 日期格式化 -->
+        <template #default="{ row }">
+          {{ formatTime(row.pub_date) }}
+        </template>
+      </el-table-column>
       <el-table-column label="状态" prop="state"></el-table-column>
       <el-table-column label="操作" width="100">
         <template #default="{ row }">
@@ -48,5 +117,20 @@ const onDeleteArticle = (row) => {
         <el-empty description="没有数据" />
       </template>
     </el-table>
+
+    <!-- 分页区域 -->
+    <el-pagination
+      v-model:current-page="params.pagenum"
+      v-model:page-size="params.pagesize"
+      :page-sizes="[2, 3, 5, 10]"
+      layout=" total, sizes, prev, pager, next"
+      :total="total"
+      @size-change="onSizeChange"
+      @current-change="onCurrentChange"
+      style="margin-top: 20px; justify-content: flex-end"
+    />
+
+    <!-- 添加编辑的抽屉 -->
+    <article-edit ref="articleEditRef"></article-edit>
   </page-container>
 </template>
